81c3ed058ec3efe91f056d9f891fb828c7ef7170
Jhonatan Reis <reisdeveloper10@gmail.com>
2024-03-15 13:46:45 -0300

    signup created

--- lib/core/auth/auth_screens/auth_signup_screen.dart
+++ lib/core/auth/auth_screens/auth_signup_screen.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+import 'dart:js';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class SignUpScreen extends StatefulWidget {
   const SignUpScreen({super.key});
@@ -8,13 +12,46 @@ class SignUpScreen extends StatefulWidget {
 }
 
 class _SignUpScreenState extends State<SignUpScreen> {
+  XFile? imgFile;
+  ImagePicker pickerImg = ImagePicker();
+
+  pickImgFromGallery() async {
+    imgFile = await pickerImg.pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      imgFile;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text("Sign Up"),
+          const SizedBox(
+            height: 11,
+          ),
+          InkWell(
+            onTap: () {
+              if (imgFile != null) {
+                pickImgFromGallery();
+              }
+            },
+            child: CircleAvatar(
+              radius: MediaQuery.of(context).size.width * 0.20,
+              backgroundColor: Colors.grey,
+              backgroundImage:
+                  imgFile == null ? null : FileImage(File(imgFile!.path)),
+              child: imgFile == null
+                  ? const Icon(
+                      Icons.add_a_photo,
+                      size: 40.0,
+                      color: Colors.white,
+                    )
+                  : null,
+            ),
+          ),
         ],
       ),
     );
